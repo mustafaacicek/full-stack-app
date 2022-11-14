7faf5b26811d2b6d9ca54cb17ee7517908f8d641
mustafacicek <mustafaciceknote8@gmail.com>
2022-11-14 22:34:21 +0300

    v1.0

---
+++ src/layout/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">
+                        Full Stack App(Spring Boot & React)
+                        
+                    </Link>
+                    <button className="navbar-toggler" type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <Link className='btn btn-outline-light' to="/adduser">Add user</Link>
+                </div>
+            </nav>
+        </div>
+    );
+}
